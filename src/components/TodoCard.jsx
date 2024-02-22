54f572898aa87493c6f6885fe647f95f01631749
mjcoding0702 <chungmangjie2000@hotmail.com>
2024-02-22 12:04:55 +0800

    hotfix: fix timer pause doesn't work

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,10 +1,38 @@
-import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 
 export default function TodoCard( { todo }) {
     const completed = todo.completed;
     const border = completed ? "success" : "danger";
+    const [timer, setTimer] = useState(0);
+    const [timerInterval, setTimerInterval] = useState(null);
 
+    // Functinos related to the timer
+    const startTimer = () => {
+      if (timerInterval === null) {
+        const intervalID = setInterval(() => {
+          setTimer((prevTimer) => prevTimer + 1);
+        }, 1000);
+        setTimerInterval(intervalID);
+      }
+    }
+
+    const pauseTimer = () => {
+      clearInterval(timerInterval);
+    }
+
+    const resetTimer = () => {
+      clearInterval(timerInterval);
+      setTimerInterval(null);
+      setTimer(0);
+    }
+
+    useEffect(() => {
+      return () => {
+        clearInterval(timerInterval);
+      }
+    }, [timerInterval])
 
     return (
       <Card border={border} className="my-3">
@@ -12,6 +40,16 @@ export default function TodoCard( { todo }) {
           <Card.Body>
             <Card.Title>{todo.title}</Card.Title>
             <Card.Text>{todo.description}</Card.Text>
+            <p>Timer: {timer} seconds</p>
+            <Button onClick={startTimer}>
+              <i className="bi bi-play"></i>
+            </Button>
+            <Button onClick={pauseTimer} className="mx-2">
+              <i className="bi bi-pause-fill"></i>
+            </Button>
+            <Button onClick={resetTimer} className="ms-2">
+              <i className="bi bi-arrow-clockwise"></i>
+            </Button>
           </Card.Body>
       </Card>
     )
